9930258fcbf19a4877136f43a762bae11d56ca33
legshampoo <daniel.c.baker@gmail.com>
2021-04-22 21:14:21 +0700

    post

--- routes/gecko.js
+++ routes/gecko.js
@@ -16,28 +16,21 @@ const coins = require('../config/coin-list.js');
 router
   .route('/coins/markets')
   .get(async (req, res) => {
+    console.log(req.query);
     // const api_url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin%2Cethereum&order=market_cap_desc&per_page=100&page=1&sparkline=false';
     var api_url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=';
-    // for(var i = 0; i < coinList.length; i++){
-    //   const id = coinlist[i];
-    //   console.log('id: ', id);
-    //   baseUrl += coinList[i]; 
-    // }
-    // console.log(coins.coinList);
+    
     coins.coinList.map((id, i) => {
-      console.log(id);
       api_url += id + '%2C';
     })
     api_url += '&order=market_cap_desc&per_page=100&page=1&sparkline=false';
     
-    console.log(api_url);
     const response = await axios.get(api_url);
     const data = response.data;
-    console.log('data: ', data);
-    // console.log(coinList);
     res.send(data);
   })
   .post((req, res) => {
+    console.log(req.query.payload);
     res.send('one post');
   });
 
