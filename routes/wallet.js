c02729ccb61b4d44d06e51580173b2c3369502e6
legshampoo <daniel.c.baker@gmail.com>
2021-07-06 00:20:06 +0700

    custom object

--- routes/wallet.js
+++ routes/wallet.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const axios = require('axios');
+const web3 = require('web3');
 const { DateTime } = require('luxon');
 const dateFormat = require('dateformat');
 
@@ -24,7 +25,11 @@ module.exports = {
     const normalTransactions = normalTxResult.data.result;
     transactions.push(...normalTransactions);
     console.log('after normal: ', transactions.length);
-    let txs = [];
+
+    console.log('first: ', transactions[0]);
+    console.log('last: ', transactions[transactions.length-1]);
+
+    let txns = [];
     transactions.map(tx => {
       const timestamp = tx.timeStamp;
       const date = new Date(timestamp*1000);
@@ -33,19 +38,72 @@ module.exports = {
       tx.time = dateFormatted;
 
       // put the time and timestamp at the top of the object keys for readability in the sheet
-      let objectOrder = {
-        'time': null,
-        'timeStamp': null,
-        'blockNumber': null
-      };
+      // let objectOrder = {
+      //   'time': null,
+      //   'timeStamp': null,
+      //   'blockNumber': null
+      // };
 
-      const newTx = Object.assign(objectOrder, tx);
+      // const newTx = Object.assign(objectOrder, tx);
       // console.log(newTx);
-      txs.push(newTx);
+      // console.log(typeof tx.tokenName);
+      let tokenName;
+      let tokenSymbol;
+      let tokenDecimal;
+      let isError;
+      if(typeof tx.tokenName === 'undefined'){
+        // console.log('GOT IT');
+        tokenName = 'null';
+        tokenSymbol = 'null';
+        tokenDecimal = 0;
+        isError = tx.isError;
+      }else {
+        tokenName = tx.tokenName;
+        tokenSymbol = tx.tokenSymbol;
+        tokenDecimal = tx.tokenDecimal;
+        isError = 'null';
+      }
+
+      // console.log('type wallet: ', typeof wallet);
+      // console.log('wallet: ', wallet);
+      // console.log('from  : ', tx.from);
+      let flow;
+      if(tx.from.toUpperCase() === wallet.toUpperCase()){
+        // console.log('from!');
+        flow = 'in';
+      }
+      if(tx.to.toUpperCase() === wallet.toUpperCase()){
+        // console.log('to!');
+        flow = 'out';
+      }
+
+      const value = web3.utils.fromWei(tx.value, 'ether');
+      // console.log('value: ', value);
+      let obj = {
+        time: tx.time,
+        timeStamp: tx.timeStamp,
+        blockNumber: tx.blockNumber,
+        contractAddress: tx.contractAddress,
+        from: tx.from,
+        to: tx.to,
+        value: value,
+        flow: flow,
+        tokenName: tokenName,
+        tokenSymbol: tokenSymbol,
+        tokenDecimal: tokenDecimal,
+        gas: tx.gas,
+        gasPrice: tx.gasPrice,
+        gasUsed: tx.gasUsed,
+        cumulativeGasUsed: tx.cumulativeGasUsed,
+        confirmations: tx.confirmations,
+        transactionIndex: tx.transactionIndex
+      }
+
+      txns.push(obj);
     })
     
-    console.log(txs[0]);
-    res.send(txs);
+    console.log(txns[0]);
+    res.send(txns);
   },
   normalTransactions: async (req, res) => {
     console.log('Get Wallet Transactionz');
